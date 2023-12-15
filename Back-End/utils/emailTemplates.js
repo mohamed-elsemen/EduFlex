920107a089777a1514f054e555734304b96e9004
MohammedElKhadrawy <mohammedelkhadrawy641@gmail.com>
2023-12-15 13:51:21 +0200

    edited email verification template

---
+++ Back-End/utils/emailTemplates.js
@@ -0,0 +1,73 @@
+const generateVerifyEmailTemplate = (otp, name) => {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Verification OTP for your account</title>
+    <style type="text/css">
+      /* Set default font styles */
+      body {
+        font-family: Arial, sans-serif;
+        font-size: 14px;
+        line-height: 1.5;
+        color: #333;
+      }
+      /* Center the content */
+      .container {
+        max-width: 600px;
+        margin: 0 auto;
+      }
+      /* Add styles to the logo */
+      .logo {
+        display: block;
+        margin: 20px auto;
+        max-width: 200px;
+        border-radius: 50%;
+        overflow: hidden;
+      }
+      /* Add styles to the subject */
+      .subject {
+        margin-top: 30px;
+        text-align: center;
+      }
+      /* Add styles to the message content */
+      .message {
+        margin-top: 20px;
+      }
+      /* Add styles to the footer */
+      .footer {
+        margin-top: 40px;
+        font-size: 12px;
+        color: #999;
+        text-align: center;
+      }
+    </style>
+  </head>
+      <body>
+      <div class="container">
+        <img class="logo" src="https://i.postimg.cc/gjXVLTbL/Edu-Flex-Logo.png" alt="Logo" />
+        <h3 class="subject">Email Verification</h3>
+        <div class="message">
+        <h4> Hello, ${name}</h4>
+        <p>Thank you for registering with EduFlex App. As part of our security measures, we require all new users to verify their account.</p>
+        <p>Your account verification code is: </p>
+        <p style="color:#00bf63;font-size:25px;letter-spacing:2px;text-align:center"><b>${otp}</b></p>
+        <p>Please enter this code on the verification page to complete the process.</p>
+        <p>Note that this code <b>expires in 1 hour(s)</b>.</p>
+        <p>If you do not verify your account within this time, you may need to request a new OTP.</p>
+        <p>If you did not register for this service, please disregard this email and contact us immediately at [eduflexteam@outlook.com].</p>
+        <p>Thank you for using EduFlex.</p>
+        <p>The EduFlex App Team.</p>
+        </div>
+        <div class="footer">
+        <p>Copyright © 2024 EduFlex App.
+        </p>
+      </div>
+        </div>
+      </body>
+    </html>
+  `;
+};
+
+module.exports = generateVerifyEmailTemplate;
--- Back-End/utils/sendVerificationEmail.js
+++ Back-End/utils/sendVerificationEmail.js
@@ -1,16 +1,12 @@
 const sendEmail = require('./sendEmail');
+const generateVerifyEmailTemplate = require('./emailTemplates');
 
 const sendVerificationEmail = async ({ name, email, otp }) => {
-  const message = `<p>Please verify your email with the code below:</p>
-  <p style="color:#00bf63;font-size:25px;letter-spacing:2px;"><b>${otp}</b></p>
-  <p>This code <b>expires in 1 hour(s)</b>.</p>`;
 
   return sendEmail({
     to: email,
     subject: 'Email Verification',
-    html: `<h4> Hello, ${name}</h4>
-    ${message}
-    `,
+    html: generateVerifyEmailTemplate(otp, name),
   });
 };
 
